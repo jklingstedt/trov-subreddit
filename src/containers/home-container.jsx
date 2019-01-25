02e2f063ea6ef629ce9fb507008916fb8f3a00bd
Jason Klingstedt <jason.klingstedt@gmail.com>
2019-01-24 16:03:30 -0800

    basic setup and api call

--- src/containers/home-container.jsx
+++ src/containers/home-container.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types' // eslint-disable-line
 
+import { getSubreddits } from '../services/get-subreddits'
+
 class HomeContainer extends Component { // eslint-disable-line
+    getSubreddits() { // eslint-disable-line
+        getSubreddits()
+    }
+
     render() { // eslint-disable-line
         return (
             <div>
                 Home
+                <button onClick={this.getSubreddits}>Get Subreddits</button>
             </div>
         )
     }
--- src/containers/main-container.jsx
+++ src/containers/main-container.jsx
@@ -1,51 +1,26 @@
-import React, { Fragment, Component } from 'react'
-import { bool, shape } from 'prop-types'
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import asyncComponent from '../components/async'
 import NavigationContainer from './navigation-container' // eslint-disable-line
 
-const AsyncLoginContainer = asyncComponent(() => import('./login-container'))
 const AsyncHomeContainer = asyncComponent(() => import('./home-container'))
 export const Async404 = asyncComponent(() => import('../components/error-404'))
 
-export class MainContainer extends Component {
-    static propTypes = {
-        user: shape({
-            isAuthed: bool.isRequired
-        }).isRequired
-    }
-
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-
-    render() {
-        return (
-            <div className="main-container">
-                {this.props.user.isAuthed ?
-                    <Router>
-                        <Fragment>
-                            <NavigationContainer />
-                            <Switch>
-                                <Route
-                                    exact
-                                    path="/"
-                                    component={AsyncHomeContainer}
-                                />
-                                <Route component={Async404} />
-                            </Switch>
-                        </Fragment>
-                    </Router>
-                    :
-                    <AsyncLoginContainer />
-                }
-            </div>
-        )
-    }
-}
+const MainContainer = () => (
+    <div className="main-container">
+        <Router>
+            <Fragment>
+                <NavigationContainer />
+                <Switch>
+                    <Route exact path="/" component={AsyncHomeContainer} />
+                    <Route component={Async404} />
+                </Switch>
+            </Fragment>
+        </Router>
+    </div>
+)
 
 export const mapStateToProps = state => ({
     user: state.user
