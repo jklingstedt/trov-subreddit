bf63dbcef457bcbf177be2979349090024e70f7b
Jason Klingstedt <jason.klingstedt@gmail.com>
2019-01-30 12:59:04 -0800

    fixing test to use non-dynamic date and adding new stories

--- src/components/__stories__/header.stories.jsx
+++ src/components/__stories__/header.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Router } from 'react-router-dom'
+
 import Header from '../header'
 import history from '../../utils/history'
 
