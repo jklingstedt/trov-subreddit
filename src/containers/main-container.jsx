e320845c6a2dffca7ea88908eae5ada5ff5ec182
Jason Klingstedt <jason.klingstedt@gmail.com>
2019-02-07 16:04:31 -0800

    updating packages and updating code splitting to use lazy()

--- src/containers/main-container.jsx
+++ src/containers/main-container.jsx
@@ -1,27 +1,39 @@
-import React, { Fragment } from 'react'
+import React, { lazy, Suspense, Fragment } from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 
 import history from '../utils/history'
-import asyncComponent from '../components/async'
+// import asyncComponent from '../components/async'
 import MainContainerElement from '../components/elements/main-container-element'
 import Header from '../components/header'
 
-const AsyncHomeContainer = asyncComponent(() => import('./home-container'))
-const AsyncSubredditContainer = asyncComponent(() => import('./subreddit-container'))
-const Async404 = asyncComponent(() => import('../components/error-404'))
+// const AsyncHomeContainer = asyncComponent(() => import('./home-container'))
+// const AsyncSubredditContainer = asyncComponent(() => import('./subreddit-container'))
+// const Async404 = asyncComponent(() => import('../components/error-404'))
+
+const AsyncHomeContainer = lazy(() => import('./home-container'))
+const AsyncSubredditContainer = lazy(() => import('./subreddit-container'))
+const Async404 = lazy(() => import('../components/error-404'))
+
+const Loading = () => 'Loading...'
 
 const MainContainer = () => (
-    <MainContainerElement>
-        <Router history={history}>
-            <Fragment>
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={AsyncHomeContainer} />
-                    <Route exact path="/r/:sub" component={AsyncSubredditContainer} />
-                    <Route component={Async404} />
-                </Switch>
-            </Fragment>
-        </Router>
-    </MainContainerElement>
+    <Suspense fallback={Loading}>
+        <MainContainerElement>
+            <Router history={history}>
+                <Fragment>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={AsyncHomeContainer} />
+                        <Route
+                            exact
+                            path="/r/:sub"
+                            component={AsyncSubredditContainer}
+                        />
+                        <Route component={Async404} />
+                    </Switch>
+                </Fragment>
+            </Router>
+        </MainContainerElement>
+    </Suspense>
 )
 export default MainContainer
