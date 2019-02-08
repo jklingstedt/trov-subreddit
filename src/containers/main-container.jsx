a9d0c96da8cff24af94bb75033551f3095c25f3f
Jason Klingstedt <jason.klingstedt@gmail.com>
2019-02-07 16:07:35 -0800

    updating test snapshot and removing commented code

--- src/containers/main-container.jsx
+++ src/containers/main-container.jsx
@@ -2,13 +2,8 @@ import React, { lazy, Suspense, Fragment } from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 
 import history from '../utils/history'
-// import asyncComponent from '../components/async'
 import MainContainerElement from '../components/elements/main-container-element'
 import Header from '../components/header'
-
-// const AsyncHomeContainer = asyncComponent(() => import('./home-container'))
-// const AsyncSubredditContainer = asyncComponent(() => import('./subreddit-container'))
-// const Async404 = asyncComponent(() => import('../components/error-404'))
 
 const AsyncHomeContainer = lazy(() => import('./home-container'))
 const AsyncSubredditContainer = lazy(() => import('./subreddit-container'))
